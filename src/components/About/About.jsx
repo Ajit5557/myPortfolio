5d6450564d7d1b8bea85dc5764bd655018964beb
Ajit Chauhan <ajitchauhan05.ac@gmail.com>
2025-12-01 18:12:23 +0530

    Updated CV link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
         </p>
 
         {/* Resume Button */}
-        <a href="https://drive.google.com/file/d/1SdN8rMPp2K02YW_PUPu3OJ4uuj1Ve8-u/view?usp=drive_link" target='_blank' rel='noopener noreferrer' className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform:hover-scale-105'
+        <a href="https://drive.google.com/file/d/1IFMmhg-8mqckdH15BspGzRBU3Dwn9-Pu/view?usp=drive_link" target='_blank' rel='noopener noreferrer' className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform:hover-scale-105'
         style={{
           background:'linear-gradient(90deg, #8245ec, #a855f7)',
           boxShadow:'0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec'
